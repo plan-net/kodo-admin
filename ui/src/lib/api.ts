c2a1d11024fd6260ca05586629fffd01e6dd5cf5
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2025-01-22 16:20:42 +0100

    Corrected type

--- ui/src/lib/api.ts
+++ ui/src/lib/api.ts
@@ -13,7 +13,7 @@ import {client, flowsFlows} from "../../ui/src/lib/gen-api";
 
 const reg_url = process.env.REGISTRY_URL || 'http://localhost:3367'
 
-confirm('env.REGISTRY_URL :' + reg_url)
+console.log('env.REGISTRY_URL :' + reg_url)
 console.log('reg_url :', reg_url)
 
 client.setConfig({
