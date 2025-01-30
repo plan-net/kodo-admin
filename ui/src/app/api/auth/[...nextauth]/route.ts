1922f8bee34a812f75e57277c3bd87f822c406c8
Jarek Jaryszew <jarek.jaryszew@gmail.com>
2025-01-30 12:20:40 +0100

    Access token is now available within application

--- ui/src/app/api/auth/[...nextauth]/route.ts
+++ ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { AuthOptions } from "next-auth";
 import NextAuth from "next-auth"
 import KeycloakProvider from "next-auth/providers/keycloak"
+
+
 export const authOptions: AuthOptions = {
     providers: [
         KeycloakProvider({
@@ -8,7 +10,22 @@ export const authOptions: AuthOptions = {
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
             issuer: process.env.KEYCLOAK_ISSUER
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+          // Persist the OAuth access_token to the token right after signin
+          if (account) {
+            token.accessToken = account.access_token
+          }
+          return token
+        },
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token from a provider.
+          (session as any).accessToken = token.accessToken
+          return session
+        }
+      }
 }
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
+
