3a00b882299aa6c68e7b0aac72ef3ff983c143c7
bebok <bebok>
2025-02-05 16:02:16 +0100

    - quick fixes

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // todo: this should be removed after types fixed
     typescript: {
         ignoreBuildErrors: true,
     },
