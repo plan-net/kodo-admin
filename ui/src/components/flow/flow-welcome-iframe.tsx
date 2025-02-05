0acba6b17e867a5c9ce202da511befe4652f594d
Bebok <Bebok>
2025-02-05 23:35:00 +0100

    html loading

--- ui/src/components/flow/flow-welcome-iframe.tsx
+++ ui/src/components/flow/flow-welcome-iframe.tsx
@@ -1,19 +1,34 @@
 'use client'
 
+import React from 'react';
+
 interface FlowWelcomeProps {
   url?: string;
 }
 
 export function FlowWelcomeIFrame({ url }: FlowWelcomeProps) {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+  }, [url]);
+
   return (
-    <div className="rounded-lg border border-border bg-background p-6 w-full h-[800px]">
+    <div className="rounded-lg border border-border bg-background p-6 w-full h-[800px] relative">
+      {isLoading && (
+        <div className="absolute inset-0 bg-black/50 z-10 flex justify-center items-center">
+          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary" />
+        </div>
+      )}
       <iframe
-        allow="same-origin" allow-scripts
+        allow="same-origin"
+        allowScripts
         src={url}
         className="w-full h-full"
         frameBorder="0"
         // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        onLoad={() => setIsLoading(false)}
       />
     </div>
   );
